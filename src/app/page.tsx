45d0ed55eb473b7f6ec1bf7c3201fda90c49a602
abdibrokhim <abdibrokhim@gmail.com>
2024-12-20 01:51:36 +0500

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/react"
 import Footer from "./components/mfooter";
 import Header from "./components/mheader";
 import IntroSection from "./sections/introSection";
@@ -9,6 +10,7 @@ import ContestSection from "./sections/contestSection";
 export default function Home() {
   return (
     <div className="grid grid-rows items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)] scroll-smooth">
+      <Analytics />
       <Header />
       <IntroSection />
       <ContestSection />
